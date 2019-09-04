b093e83e1676f59c79c709667043d25d700fc75e
Matthew Nicholas <M4TMBP@MBP.local>
2019-09-04 11:59:59 +0100

    changed the welcome file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kodflix for MN.
+          <h1>Kodflix for MN!</h1>
         </p>
         <a
           className="App-link"
